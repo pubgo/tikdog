4c2316c4f2999145367e0a86f854693c015e9d04
白云辉 <kooksee@163.com>
2020-12-06 19:53:42 +0800

    fix: update

--- tikdog_sync/wait.go
+++ tikdog_sync/wait.go
@@ -3,7 +3,18 @@ package tikdog_sync
 import (
 	"github.com/pubgo/xlog"
 	"go.uber.org/atomic"
+	"math/rand"
+	"sync"
+	"time"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func Range(min, max int) int {
+	return min + rand.Intn(max-min)
+}
 
 func NewWaiter() *Waiter {
 	return &Waiter{
@@ -13,6 +24,7 @@ func NewWaiter() *Waiter {
 }
 
 type Waiter struct {
+	mu   sync.Mutex
 	data map[string]*atomic.Uint32
 	skip map[string]*atomic.Uint32
 }
@@ -25,6 +37,9 @@ func (t *Waiter) check(key string) {
 }
 
 func (t *Waiter) Report(key string, c *atomic.Uint32) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
 	t.check(key)
 
 	if c.Load() != 0 {
@@ -36,6 +51,9 @@ func (t *Waiter) Report(key string, c *atomic.Uint32) {
 }
 
 func (t *Waiter) Skip(key string) bool {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
 	t.check(key)
 
 	if t.data[key].Load() == 0 {
@@ -44,7 +62,7 @@ func (t *Waiter) Skip(key string) bool {
 	}
 
 	t.skip[key].Inc()
-	if t.skip[key].Load() > 5 {
+	if t.skip[key].Load() > uint32(Range(5, 120)) {
 		t.skip[key].Store(0)
 		xlog.Debug("no skip")
 		return false
